90d3cdeb57a4addcc1740193a051765b65abc8c2
NHS-Ryan <ryan.lord@nhs.net>
2026-05-23 21:20:03 +0100

    Update main.R
    Completed map_questions.R

--- R/main.R
+++ R/main.R
@@ -7,16 +7,16 @@ library(readxl)
 source("R/config.R")
 source("R/import_raw_data.R")
 source("R/map_teams.R")
+source("R/map_questions.R")
 source("R/anonymity_suppression.R")
 # source("R/calculate_themes.R")
-# source("R/map_questions.R)
 source("R/write_outputs.R")
 
 # Run pipeline
 vars <- config()
 files <- import_raw_data()
 map_teams(files)
-# map_questions()
+map_questions(files)
 # calculate_themes()
 files$ox_q_aggregate_results <- anonymity_suppression(
   files$ox_q_aggregate_results,
@@ -27,4 +27,4 @@ write_outputs(files)
 # Run tests
 testthat::test_dir("tests/testthat")
 
-# remove(files,vars)
+remove(files,vars)
