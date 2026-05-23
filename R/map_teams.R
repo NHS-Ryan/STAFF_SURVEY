0910de93e69c11a8556f03d35a298dbd5f413499
NHS-Ryan <ryan.lord@nhs.net>
2026-05-23 13:06:36 +0100

    Create map_teams.R
    Checks that list of teams in maps/ox_teams.csv & ox_q_aggregate_results, ox_q_option_results & ox_theme_results match.

---
+++ R/map_teams.R
@@ -0,0 +1,37 @@
+# Function to check whether there are new teams this year, and if to prompt user
+# to add them to the ox_teams.csv file with appropriate information.
+
+map_teams <- function(files) {
+
+  teams_current <- c(files$ox_teams$team_full)
+
+  teams_new <- bind_rows(
+
+  files$ox_q_aggregate_results %>%
+    filter(DimName == "BD3") %>%
+    select(DimValue) %>%
+    distinct(),
+
+  files$ox_q_option_results %>%
+    filter(DimName == "BD3") %>%
+    select(DimValue) %>%
+    distinct(),
+
+  files$ox_theme_results %>%
+    filter(DimName == "BD3") %>%
+    select(DimValue) %>%
+    distinct()
+
+  ) %>%
+    distinct()
+
+  team_diff <- setdiff(pull(teams_new),teams_current)
+
+  if(length(team_diff) > 0) {
+    print(team_diff)
+    stop("New teams present: see above. Add to ox_teams.csv with appropriate details then rerun main.R")
+  }
+
+}
+
+
