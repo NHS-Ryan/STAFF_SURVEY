3d8c06f0ef2068cd9e2f07cbe61c2295ce406432
NHS-Ryan <ryan.lord@nhs.net>
2026-06-01 16:38:42 +0100

    Update config.R
    New global variable that tells you what national staff survey years have already been processed during previous runs of this repo.

--- R/config.R
+++ R/config.R
@@ -1,10 +1,20 @@
-
 config <- function(){
+
+  # size of staff groups to exclude from individual question results
   suppression_threshold <- 3
+
+  # List of years that we already have national staff survey dataset results
+  # from
+  nat_results_files <- list.files("data/national_results/")
+  complete_years <-
+    substr(nat_results_files,
+         nchar(nat_results_files)-7,
+         nchar(nat_results_files)-4)
+
   return(
     list(
-      suppression_threshold = suppression_threshold
+      suppression_threshold = suppression_threshold,
+      complete_years = complete_years
     )
   )
 }
-
