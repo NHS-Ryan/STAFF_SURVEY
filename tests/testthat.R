e5f3af721807c1ad6bdc630f77af0f27fbfa7991
NHS-Ryan <ryan.lord@nhs.net>
2026-06-08 17:21:38 +0100

    tests\testthat replaced by setup.R
    Replaced by tests\testthat\setup.R. setup.R expanded to create correct environment before testing undertaken.

--- tests/testthat.R
+++
@@ -1,6 +0,0 @@
-library(testthat)
-
-vars <- config()
-suppression_threshold <- vars$suppression_threshold
-
-test_check("STAFF_SURVEY")
--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,5 +1,17 @@
+library(tidyverse)
+library(readxl)
+library(here)
 
-source("../../R/config.R")
+setwd(here::here())
+
+r_files <- list.files("R", full.names = TRUE, pattern = "\\.[Rr]$")
+r_files <- r_files[basename(r_files) != "main.R"]
+
+lapply(r_files, source)
 
 vars <- config()
+files <- import_raw_data()
+files <- anonymity_suppression(files,vars)
 suppression_threshold <- vars$suppression_threshold
+theme_inputs <- prepare_theme_results_inputs(files)
+calculated_themes <- calculate_themes(files)
