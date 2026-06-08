a156321bee11bd97b93e416e7f080ae1f1931476
NHS-Ryan <ryan.lord@nhs.net>
2026-06-08 17:16:51 +0100

    Update main.R
    Added in additional mapping files and some minor tidying up.

--- R/main.R
+++ R/main.R
@@ -2,29 +2,45 @@
 library(tidyverse)
 library(testthat)
 library(readxl)
+library(here)
 
 # Load functions
 source("R/config.R")
 source("R/import_raw_data.R")
-source("R/map_teams.R")
+source("R/map_dims.R")
+source("R/map_options.R")
 source("R/map_questions.R")
+source("R/map_teams.R")
+source("R/map_themes.R")
 source("R/anonymity_suppression.R")
-# source("R/calculate_themes.R")
+source("R/calculate_themes.R")
 source("R/write_outputs.R")
 
-# Run pipeline
+# Load environment variables
 vars <- config()
+
+# Import data
 files <- import_raw_data()
-map_teams(files)
+
+# Check if there are new options, questions, teams or themes this year
+# Each change will need to be resolved by editing files in maps/
+# Note that map_dims() will automatically stop execution and give a message that
+# certain dims are being dropped. These should be reviewed before continuing.
+# Only those dims needed in the dashboard should be retained.
+map_dims(files)
+map_options(files)
 map_questions(files)
-# calculate_themes()
-files$ox_q_aggregate_results <- anonymity_suppression(
-  files$ox_q_aggregate_results,
-  vars
-)
-write_outputs(files)
+map_teams(files)
+map_themes(files)
+
+# Calculate locally agreed theme results
+files$ox_theme_results <- calculate_themes(files)
+
+# Enforce anonymity suppression rules
+files <-  anonymity_suppression(files, vars)
 
 # Run tests
 testthat::test_dir("tests/testthat")
 
-remove(files,vars)
+# Save files
+write_outputs(files)
