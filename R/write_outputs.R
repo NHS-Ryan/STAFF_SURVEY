13385b48935bb3e2437c18cffe9b95bc2bb53d12
NHS-Ryan <ryan.lord@nhs.net>
2026-06-08 17:20:04 +0100

    Update write_outputs.R
    Typos changed and additional files added to write.

--- R/write_outputs.R
+++ R/write_outputs.R
@@ -1,9 +1,9 @@
-
+# write final outputs to data/
 
 write_outputs <- function(files) {
-  write.csv(files$nat_results,"data/national_results.csv")
-  write.csv(files$ox_q_aggregate_results,"data/positive_scoring_suppressed.csv")
+  write.csv(files$nat_result_themes,"data/nat_results_themes.csv")
+  write.csv(files$nat_result_scores,"data/nat_results_scores.csv")
+  write.csv(files$ox_q_aggregate_results,"data/ox_q_aggregate_results.csv")
   write.csv(files$ox_q_option_results,"data/ox_q_option_results.csv")
   write.csv(files$ox_theme_results,"data/ox_theme_results.csv")
 }
-
