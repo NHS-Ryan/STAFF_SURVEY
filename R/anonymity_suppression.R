116c0df901f4b93b315349cce222470d3f6409c7
NHS-Ryan <ryan.lord@nhs.net>
2026-06-08 17:26:03 +0100

    Additional suppression
    Anonymity suppression exited to use new variable names.
    Also added in that ox_q_option_results will only have "comparator" or "organisation" level data inside of it.

--- R/anonymity_suppression.R
+++ R/anonymity_suppression.R
@@ -1,15 +1,19 @@
-
 # Suppresses individual questions in ox_q_aggregate_results where the number of
 # respondents falling into either the positive / negative respondent group is
 # below suppression_threshold (defined in config.R)
 
-anonymity_suppression <- function(df,vars) {
-  df %>%
+anonymity_suppression <- function(files,vars) {
+  files$ox_q_aggregate_results <- files$ox_q_aggregate_results %>%
     mutate(
-      pos = BaseSize * Score,
-      neg = BaseSize * (1 - Score),
-      Score = if_else(pos <= vars$suppression_threshold | neg <= vars$suppression_threshold, NA_real_,Score)
+      pos = n * score,
+      neg = n * (1 - score),
+      score = if_else(pos <= vars$suppression_threshold | neg <= vars$suppression_threshold, NA_real_,score)
     ) %>%
-    select(-c("pos","neg")) %>%
-    return()
+    select(-c("pos","neg"))
+
+  files$ox_q_option_results <- files$ox_q_option_results %>%
+    filter(dim_sub %in% c("Comparator","Organisation") | dim == "Directorate")
+
+  return(files)
 }
+
