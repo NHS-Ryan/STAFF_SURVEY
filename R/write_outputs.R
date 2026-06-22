0f1210f34b780624cb9708acb9f7b7d1f7e1aecc
NHS-Ryan <ryan.lord@nhs.net>
2026-06-22 16:29:11 +0100

    Update write_outputs.R
    Sanitizes outputs in order to stop encording errors.

--- R/write_outputs.R
+++ R/write_outputs.R
@@ -1,9 +1,49 @@
 # write final outputs to data/
 
-write_outputs <- function(files) {
-  write.csv(files$nat_result_themes,"data/nat_results_themes.csv")
-  write.csv(files$nat_result_scores,"data/nat_results_scores.csv")
-  write.csv(files$ox_q_aggregate_results,"data/ox_q_aggregate_results.csv")
-  write.csv(files$ox_q_option_results,"data/ox_q_option_results.csv")
-  write.csv(files$ox_theme_results,"data/ox_theme_results.csv")
+sanitize_utf8_df <- function(df) {
+  df %>%
+    mutate(
+      across(
+        where(is.character),
+        ~ iconv(.x, from = "", to = "UTF-8", sub = "")
+      )
+    )
 }
+
+write_outputs <- function(files) {
+  write.csv(
+    sanitize_utf8_df(files$nat_result_themes),
+    "data/nat_result_themes.csv",
+    row.names = FALSE,
+    fileEncoding = "UTF-8"
+  )
+
+  write.csv(
+    sanitize_utf8_df(files$nat_result_scores),
+    "data/nat_result_scores.csv",
+    row.names = FALSE,
+    fileEncoding = "UTF-8"
+  )
+
+  write.csv(
+    sanitize_utf8_df(files$ox_q_aggregate_results),
+    "data/ox_q_aggregate_results.csv",
+    row.names = FALSE,
+    fileEncoding = "UTF-8"
+  )
+
+  write.csv(
+    sanitize_utf8_df(files$ox_q_option_results),
+    "data/ox_q_option_results.csv",
+    row.names = FALSE,
+    fileEncoding = "UTF-8"
+  )
+
+  write.csv(
+    sanitize_utf8_df(files$ox_theme_results),
+    "data/ox_theme_results.csv",
+    row.names = FALSE,
+    fileEncoding = "UTF-8"
+  )
+}
+
