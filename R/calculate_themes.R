6f2ce4573b68650206188663f4e41f28ad3afdf5
NHS-Ryan <ryan.lord@nhs.net>
2026-06-22 16:27:01 +0100

    Update calculate_themes.R

--- R/calculate_themes.R
+++ R/calculate_themes.R
@@ -19,15 +19,54 @@ prepare_theme_results_inputs <- function(files) {
 
 }
 
-calculate_theme_results <- function(new_theme_results_inputs) {
+prepare_theme_results_inputs_nat <- function(files) {
+  questions <- files$question_scores_map
+
+  df_q <- files$nat_result_scores %>%
+    left_join(questions, by = c("q_text" = "q_text")) %>%
+    rename(q_id = q_id.y)
+
+  themes <- files$theme_questions_map %>%
+    filter(!theme %in% c("People's Promise", "Other")) %>%
+    mutate(subdomain = replace_na(subdomain, "No subdomain"))
+
+  list(df_q = df_q, themes = themes)
+}
+
+calculate_theme_results_nat <- function(files) {
+  x <- prepare_theme_results_inputs_nat(files)
+
+  x$df_q %>%
+    left_join(x$themes, by = "q_id", relationship = "many-to-many") %>%
+    filter(!is.na(theme_id)) %>%
+    group_by(year, org_id, org_name, org_type, theme_id) %>%
+    summarise(
+      score = if (all(is.na(score))) NA_real_ else mean(score, na.rm = TRUE),
+      .groups = "drop"
+    )
+}
+
+calculate_theme_results_ox <- function(new_theme_results_inputs) {
   new_theme_results_inputs$df_q %>%
-    left_join(new_theme_results_inputs$themes, by = c("q_id" = "q_id"), relationship = "many-to-many") %>%
+    left_join(new_theme_results_inputs$themes, by = "q_id", relationship = "many-to-many") %>%
+    filter(!is.na(theme_id)) %>%
     group_by(year, theme, domain, subdomain, dim, dim_sub) %>%
-    summarise(score = mean(score, na.rm = TRUE), .groups = "drop")
+    summarise(
+      score = if (all(is.na(score))) NA_real_ else mean(score, na.rm = TRUE),
+      .groups = "drop"
+    )
 }
 
 calculate_themes <- function(files) {
-  calculate_theme_results(prepare_theme_results_inputs(files))
-}
+  files$ox_theme_results <- calculate_theme_results_ox(
+    prepare_theme_results_inputs(files)
+  )
 
+  files$nat_result_themes <- bind_rows(
+    files$nat_result_themes,
+    calculate_theme_results_nat(files)
+  )
+
+  files
+}
 
